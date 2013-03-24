8a3dcd74b69f106ba6b9f1613a13f0eb13507fb3
Steven Ko <chunyuko85@gmaill.com>
2013-03-24 09:36:30 +0800

    API get categories & areas

---
+++ src/com/ptt/food/blog/api/PttFoodAPI.java
@@ -0,0 +1,83 @@
+package com.ptt.food.blog.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.ptt.food.blog.entity.Category;
+
+public class PttFoodAPI {
+    final static String         HOST  = "http://106.187.52.8";
+    public static final String  TAG   = "PttFoodAPI";
+    public static final boolean DEBUG = true;
+
+    public static ArrayList<Category> getMainCategories() {
+        return Category.getCategories();
+    }
+
+    public static ArrayList<Category> getAreaCateogories() {
+        return Category.getAreas();
+    }
+
+    public static String getMessageFromServer(String requestMethod, String apiPath, JSONObject json) {
+        URL url;
+        try {
+            url = new URL(HOST + apiPath);
+            if (DEBUG)
+                Log.d(TAG, "URL: " + url);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(requestMethod);
+
+            connection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+            if (requestMethod.equalsIgnoreCase("POST"))
+                connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.connect();
+
+            if (requestMethod.equalsIgnoreCase("POST")) {
+                OutputStream outputStream;
+
+                outputStream = connection.getOutputStream();
+                if (DEBUG)
+                    Log.d("post message", json.toString());
+
+                outputStream.write(json.toString().getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder lines = new StringBuilder();
+            ;
+            String tempStr;
+
+            while ((tempStr = reader.readLine()) != null) {
+                lines = lines.append(tempStr);
+            }
+            if (DEBUG)
+                Log.d("MOVIE_API", lines.toString());
+
+            reader.close();
+            connection.disconnect();
+
+            return lines.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
